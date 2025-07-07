5afef96bb9c451114e0166b10711cf32c5b74ba3
prakriti12-dev <gyawaliprakriti4@gmail.com>
2025-07-07 13:54:22 +0545

    here

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react"
-function Home() { 
+function Home() {
     return (
         <div>
-
+            here
         </div>
     )
 }
-export default  Home
+export default Home
